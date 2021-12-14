e5d67a4df083ab35a2927c1172020ebb0f498b2d
oleg.voloshin <oleg.voloshin@softlex.pro>
2021-12-14 16:19:34 +0300

    example

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = '4d!2@*cmd5)xz$s#jt9xt8dcox2om!78a^=+wy!dyyf)9b!lkj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'testserver',
-    'localhost'
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,14 +80,6 @@ DATABASES = {
         'USER': 'database1_role',
         'PASSWORD': 'database1_password',
         'HOST': 'database1',
-        'PORT': '5432',
-    },
-    'database2': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database2',
-        'USER': 'database2_role',
-        'PASSWORD': 'database2_password',
-        'HOST': 'database2',
         'PORT': '5432',
     }
 }
